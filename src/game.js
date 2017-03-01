ee5277af9958194e73582e15c21ea2bedab92caf
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-01 16:58:02 -0500

    added stats object with player objects inside

--- src/game.js
+++ src/game.js
@@ -1,1 +1,21 @@
 let runGame = require("./deck.js");
+
+
+
+
+
+let gameStats = {
+
+  let playerJuan = {
+    name: "Juan",
+    numberOfWins: 10,
+    winRatio: 0.38
+  }
+
+  let playerTwo = {
+    name: "Kerrigan",
+    numberOfWins: 13,
+    winRatio: 0.5,
+  }
+  
+}

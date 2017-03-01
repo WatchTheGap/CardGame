036a8bf8b75a2f8a650dbc06a63b9745aed14c98
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-01 15:14:28 -0500

    added ranodom number generator and counter loop

--- src/deck.js
+++ src/deck.js
@@ -1,6 +1,26 @@
-for deck = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A",
-  2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A",
-  2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A",
-  2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A", ];
+// for deckOfCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-  
+function shuffle(x) {
+  let deckA = [];
+  let cardA;
+  for (let i = 1; i <= x; i++) {
+    if (i >= 1) {
+      console.log(i)
+    }
+    else {
+      console.log("cats");
+    }
+  }
+
+  // push.deckA(cardA);
+
+};
+
+
+function random(maximum) {
+  return Math.ceil(Math.random() * maximum);
+};
+
+// console.log(random(52));
+
+shuffle(random(15));

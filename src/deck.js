2bb4db731d0805a05b34a8bbdb40d1c26b71fcf0
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-01 16:32:54 -0500

    added require deck to game file

--- src/deck.js
+++ src/deck.js
@@ -1,30 +1,22 @@
 
-module.exports = function buildDeck(a) {
   let deckOfCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
   let shuffleDeck = [];
+
   function random(maximum) {
     return Math.ceil(Math.random() * maximum);
   };
 
-  shuffleDeck.push(deckOfCards[random(deckOfCards.length)]);
-  console.log(shuffleDeck);
+  module.exports = function buildDeck(a) {
+    return deckOfCards;
+  // shuffleDeck.push(deckOfCards[random(deckOfCards.length)]);
 };
 
 
-//
-//
-//   for (let i = 1; i <= x; i++) {
-//     if (i >= 1) {
-//       console.log(i);
-//       console.log(deckOfCards[i]);
-//     }
-//   }
-// };
 
-// function random(maximum) {
-//   return Math.floor(Math.random() * deckOfCards.length);
-// };
-//
-//
-// shuffle(deckOfCards[random(deckOfCards.length)]);
+
+  for (let i = 1; i <= deckOfCards.length; i++) {
+    if (i >= 1) {
+      console.log(i);
+      console.log(deckOfCards[i]);
+    };
+  };
---
+++ src/game.js
@@ -0,0 +1,1 @@
+let runGame = require("./deck.js");

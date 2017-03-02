6dedc80fc3ffbfe712c9ff6e4d2fc92bd1c816da
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-01 19:04:35 -0500

    added tracking variables for number of wins and ties.

--- src/deck.js
+++ src/deck.js
@@ -1,6 +1,8 @@
 
-  let deckOfCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  let shuffleDeck = [];
+  let deckOfCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
   function random(maximum) {
     return Math.floor(Math.random() * maximum);
@@ -8,33 +10,34 @@
 
   module.exports = function buildDeck(a) {
     return deckOfCards;
-  // shuffleDeck.push(deckOfCards[random(deckOfCards.length)]);
 };
 
 
 
 let handA = [];
 let handB = [];
+let playerOne = 0;
+let playerTwo = 0;
+let ties = 0;
 
 deckOfCards.forEach(function deal(newCard) {
   handA.push(newCard * random(deckOfCards.length));
   handB.push(newCard * random(deckOfCards.length));
 
-
 if (handA[handA.length - 1] === handB[handB.length - 1]) {
+  ties = ties + 1;
   console.log("tie");
 }
   else if (handA[handA.length - 1] > handB[handB.length-1]) {
+    playerTwo = playerTwo + 1;
+    console.log(playerTwo);
     console.log("A wins");
   }
   else {
+    playerOne = playerOne + 1;
+    console.log(playerOne);
     console.log("B wins");
   }
-  // else {
-  //   console.log("tie");
-  // }
-  // return console.log(handA, handB);
-
 });
 
 
